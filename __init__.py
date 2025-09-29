3f88a4abe4139add944cece2f4e889b557348474
Johan Enstam <johan.enstam98@gmail.com>
2025-09-29 18:47:40 +0200

    chore: Lägg till __init__.py-filer för paketstruktur

---
+++ __init__.py
@@ -0,0 +1,5 @@
+"""
+IGDB Game Recommender - Root Package
+
+Detta är rotpaketet för IGDB Game Recommender-projektet.
+"""
---
+++ data-pipeline/__init__.py
@@ -0,0 +1,5 @@
+"""
+IGDB Game Recommender - Data Pipeline Package
+
+Detta paket innehåller moduler för datainsamling, bearbetning och analys av IGDB-speldata.
+"""
